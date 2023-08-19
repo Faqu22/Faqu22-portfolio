edb7b1fe6c2573c29329b7602673ebc705aee242
Faqu22 <facundoalvarez415@gmail.com>
2023-08-19 00:06:35 -0300

    Creation of component Type

---
+++ src/components/Home/Type.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Software Developer",
+          "Backend Developer",
+          "Effective Communicator",
+          "Soft Skills Maven",
+          "CRUD Operations Knowledgeable",
+          "Backend problem solver",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
