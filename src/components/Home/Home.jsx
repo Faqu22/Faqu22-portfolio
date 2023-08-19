c3de8dfbda0f6daeeb51d172242bc830540f46b1
Faqu22 <facundoalvarez415@gmail.com>
2023-08-19 20:27:23 -0300

    Change by background

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,16 +5,16 @@ import Bg from '../../assets/bg.jpg';
 
 function Home() {
     return (
-        <div className=" relative overflow-hidden">
+        <div className=" section relative overflow-hidden">
             <img src={Bg} className="absolute opacity-20 "></img>
             <div className="flex justify-around pt-28 items-center ">
                 <div className="text-left ml-24 mb-32">
                     <h1 className="mb-6 relative">
                         Hi There!
                     </h1>
-                    <h1 >
+                    <h1 className="relative">
                         I'm
-                        <strong className="text-pink-700 relative"> Facundo Alvarez</strong>
+                        <strong className="text-pink-700"> Facundo Alvarez</strong>
                     </h1>
                     <div className="text-2xl font-semibold relative p-10  px-1">
                         <Type />
