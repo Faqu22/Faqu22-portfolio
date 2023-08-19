ac73d48fd9cbfd9250e39e05056840928a3287da
Faqu22 <facundoalvarez415@gmail.com>
2023-08-19 00:02:59 -0300

    Creation of Loader

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,37 @@ import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home.jsx";
 import Footer from './components/Footer';
+import About from './components/About';
+import Loader from './components/Loader';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+    Navigate
+  } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
+
 
 export default function App() {
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
+    }, []);
+  
     return (
-        <div className='bg-custom-color min-h-screen'>
+        <Router>
+        {loading && <Loader />}
+        <div className="bg-custom-color min-h-screen">
             <Header />
-            <Home />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/About" element={<About />} />
+            </Routes>
             <Footer />
         </div>
+        </Router>
     )
   }
---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import AnimatedSVG from '../../src/assets/loader.svg';
+
+function Loader() {
+  return (
+    <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-blue-900 z-50">
+        <img src={AnimatedSVG} alt="Loading" className='w-40' />
+    </div>
+  );
+}
+
+export default Loader;
