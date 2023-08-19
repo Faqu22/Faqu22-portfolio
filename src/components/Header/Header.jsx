662afd0f1cf60e96bf177500ff522e55e660479d
Faqu22 <facundoalvarez415@gmail.com>
2023-08-19 20:28:01 -0300

    Add Projects route

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import logoImage from '../../assets/name.png';
+import logoImage from '../../assets/name.svg';
 import homeIcon from '../../assets/home-icon.png';
 import aboutIcon from '../../assets/about-icon.png';
 import projectIcon from '../../assets/project-icon.png';
@@ -10,19 +10,19 @@ import Icon from './Icon';
 
 function Header() {
     return (
-        <div className="h-full w-full flex items-center justify-around space-x-80">
+        <div className=" relative bg-violet-900 bg-opacity-10 h-full w-full flex items-center justify-around space-x-80">
             <a href="/">
                 <img src={logoImage} alt="Name" className="h-16 w-30 object-cover mr-10 ml-5" />
             </a>
             <nav className="flex space-x-10 text-lg"> 
             <Icon name="Home" icon={homeIcon} path="/"/>
             <Icon name="About" icon={aboutIcon} path="/About"/>
-            <Icon name="Projects" icon={projectIcon} />
+            <Icon name="Projects" icon={projectIcon} path="/Projects"/>
             <Icon name="Resume" icon={resumeIcon} />
-                <button role="link" className="flex items-center border-b-4 rounded-3xl py-1 px-2 border-transparent hover:border-blue-500 transition duration-300">
-                    <img src={githubIcon} alt="Github Icon" className="h-6 mr-3" />
-                    <img src={githubCat} alt="Github Cat" className="h-6 mr-3" />
-                </button>
+                <a href="https://github.com/Faqu22/Faqu22-Portfolio" role="link" className="flex items-center border-b-4 rounded-3xl py-1 px-2 border-transparent hover:border-blue-500 transition duration-300">
+                    <img src={githubIcon} alt="Github Icon" className="h-6 pr-3 pl-3" />
+                    <img src={githubCat} alt="Github Cat" className="h-6 pr-3" />
+                </a>
             </nav>
         </div>
     )
