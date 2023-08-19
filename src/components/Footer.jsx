1ac773e103670d182e6a3ad536e45c7a175a9f17
Faqu22 <facundoalvarez415@gmail.com>
2023-08-19 00:04:13 -0300

    Implementation of esthetic

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,21 @@
-import React from "react";
-import { AiFillGithub, AiOutlineTwitter, AiFillInstagram } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+import React from 'react';
+import { AiFillGithub } from 'react-icons/ai';
+import { FaLinkedinIn } from 'react-icons/fa';
 
 function Footer() {  
   return (
-    <div className="footer  flex justify-center align-sub bg-black">
-      <div>
-      </div>
-      <div>
-        <a href="https://github.com/Faqu22" target="_blank" rel="noopener noreferrer">
+    <div className="footer flex justify-evenly items-center bg-black bg-opacity-50 text-white p-6 fixed bottom-0 w-full space-x-4">
+        <div className="footer-copywright left-6 absolute">
+          <h3>Designed and Developed by Facundo Alvarez</h3>
+        </div>
+        <div className='flex items-center absolute right-6'>
+        <a href="https://github.com/Faqu22" target="_blank" rel="noopener noreferrer" className="text-xl hover:text-blue-500 transition duration-300 mr-10">
           <AiFillGithub />
         </a>
-        <a href="https://www.linkedin.com/in/facundo-alvarez4/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.linkedin.com/in/facundo-alvarez4/" target="_blank" rel="noopener noreferrer" className="text-xl hover:text-blue-500 transition duration-300">
           <FaLinkedinIn />
         </a>
-      </div>
+        </div>
     </div>
   );
 }
