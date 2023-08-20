9a2ccdfb002130a67d153eb7b8c6da3e17dab5ea
Faqu22 <facundoalvarez415@gmail.com>
2023-08-20 12:44:31 -0300

    Remove the library react-particles, only used tsparticles

--- src/layout.jsx
+++ src/layout.jsx
@@ -2,29 +2,33 @@ import './App.css';
 import Header from "./components/Header";
 import Footer from './components/Footer';
 import Loader from './components/Loader';
-import React, { useState, useEffect, useCallback } from 'react';
-import Particles from "react-particles";
+import React, { useState, useEffect } from 'react';
+import { tsParticles } from "tsparticles";
 import particlesOptions from "./particles.json";
-import { loadFull } from "tsparticles";
 
 export default function Layout(props) {
     const [loading, setLoading] = useState(true);
-    const particlesInit = useCallback(main => {
-        loadFull(main);
-    }, [])
+
     useEffect(() => {
-      setTimeout(() => {
-        setLoading(false);
-      }, 500);
+        tsParticles.load("tsparticles", particlesOptions);
+
+        setTimeout(() => {
+            setLoading(false);
+        }, 500);
+
+        // Limpiar las partículas cuando el componente se desmonte
+        return () => {
+            tsParticles.domItem(0)?.destroy();
+        };
     }, []);
-  
+
     return (
         <div className="section pt-0 min-h-screen">
             {loading && <Loader />}
-            <Particles options={particlesOptions} init={particlesInit}/>
+            <div id="tsparticles" style={{ position: "absolute", width: "100%", height: "100%" }}></div>
             <Header />
             { props.children }
             <Footer />
         </div>
-    )
-  }
+    );
+}
