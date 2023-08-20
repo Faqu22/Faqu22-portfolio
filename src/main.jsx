18dfb2cb2596bb3fb0b424285c1a3b3120bc450e
Faqu22 <facundoalvarez415@gmail.com>
2023-08-19 22:03:11 -0300

    Implement HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
 import './index.css'
-
+import { createHashRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Projects from './pages/Projects.jsx'
+import About from './pages/About.jsx'
+const router = createHashRouter([
+    {
+        path: '/',
+        element: <Home />
+    },
+    {
+        path: '/about',
+        element: <About />
+    },
+    {
+        path: '/projects',
+        element: <Projects />
+    }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+        <RouterProvider router={router} />
   </React.StrictMode>,
 )
