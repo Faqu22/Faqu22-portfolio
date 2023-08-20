3d198dd9ef29ab6efb9b8198e86f790eaca9c137
Faqu22 <facundoalvarez415@gmail.com>
2023-08-20 13:08:10 -0300

    Fix error of background of particle

--- src/layout.jsx
+++ src/layout.jsx
@@ -2,33 +2,29 @@ import './App.css';
 import Header from "./components/Header";
 import Footer from './components/Footer';
 import Loader from './components/Loader';
-import React, { useState, useEffect } from 'react';
-import { tsParticles } from "tsparticles";
+import React, { useState, useEffect, useCallback } from 'react';
+import { Particles } from "react-tsparticles";
 import particlesOptions from "./particles.json";
+import { loadFull } from "tsparticles";
 
 export default function Layout(props) {
     const [loading, setLoading] = useState(true);
-
+    const particlesInit = useCallback(main => {
+        loadFull(main);
+    }, [])
     useEffect(() => {
-        tsParticles.load("tsparticles", particlesOptions);
-
-        setTimeout(() => {
-            setLoading(false);
-        }, 500);
-
-        // Limpiar las partículas cuando el componente se desmonte
-        return () => {
-            tsParticles.domItem(0)?.destroy();
-        };
+      setTimeout(() => {
+        setLoading(false);
+      }, 500);
     }, []);
-
+  
     return (
         <div className="section pt-0 min-h-screen">
             {loading && <Loader />}
-            <div id="tsparticles" style={{ position: "absolute", width: "100%", height: "100%" }}></div>
+            <Particles options={particlesOptions} init={particlesInit}/>
             <Header />
             { props.children }
             <Footer />
         </div>
-    );
-}
+    )
+  }
