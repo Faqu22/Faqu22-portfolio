9a5b2a55c5de966ccdef075c56594f93cecde5d4
Faqu22 <facundoalvarez415@gmail.com>
2023-08-21 12:31:08 -0300

    Some fixes in layout.

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,46 +1,30 @@
 import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
 import Loader from './components/Loader'
 import React, { useState, useEffect, useCallback } from 'react'
+import { Particles } from 'react-tsparticles'
 import particlesOptions from './particles.json'
 import { loadFull } from 'tsparticles'
-import Loadable from 'react-loadable'
-
-const AsyncHeader = Loadable({
-    loader: () => import('./components/Header'),
-    loading: Loader,
-})
-const AsyncChild = Loadable({
-    loader: () => {
-        props.children
-    },
-    loading: Loader,
-})
-
-const AsyncFooter = Loadable({
-    loader: () => import('./components/Footer'),
-    loading: Loader,
-})
-
-const AsyncParticles = Loadable({
-    loader: () => import('react-tsparticles'),
-    loading: Loader,
-    render(loaded, props) {
-        const Particles = loaded.Particles
-        return <Particles options={particlesOptions} init={particlesInit} {...props} />
-    },
-})
 
 export default function Layout(props) {
+    const [loading, setLoading] = useState(true)
     const particlesInit = useCallback((main) => {
         loadFull(main)
+    }, [])
+    useEffect(() => {
+        setTimeout(() => {
+            setLoading(false)
+        }, 500)
     }, [])
 
     return (
         <div className="section pt-0 min-h-screen">
-            <AsyncParticles />
-            <AsyncHeader />
-            <AsyncChild />
-            <AsyncFooter />
+            {loading && <Loader />}
+            <Particles options={particlesOptions} init={particlesInit} />
+            <Header />
+            {props.children}
+            <Footer />
         </div>
     )
 }
