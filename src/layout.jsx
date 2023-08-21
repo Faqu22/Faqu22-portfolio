5dc7e436f3389d1b38df4a56af809423af0368b6
Faqu22 <facundoalvarez415@gmail.com>
2023-08-21 12:42:12 -0300

    Implement Lazy loader system.

--- src/layout.jsx
+++ src/layout.jsx
@@ -1,26 +1,18 @@
 import './App.css'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Loader from './components/Loader'
 import React, { useState, useEffect, useCallback } from 'react'
 import { Particles } from 'react-tsparticles'
 import particlesOptions from './particles.json'
 import { loadFull } from 'tsparticles'
 
 export default function Layout(props) {
-    const [loading, setLoading] = useState(true)
     const particlesInit = useCallback((main) => {
         loadFull(main)
-    }, [])
-    useEffect(() => {
-        setTimeout(() => {
-            setLoading(false)
-        }, 500)
     }, [])
 
     return (
         <div className="section pt-0 min-h-screen">
-            {loading && <Loader />}
             <Particles options={particlesOptions} init={particlesInit} />
             <Header />
             {props.children}
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,49 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import Loader from './components/Loader'
 import { createHashRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Projects from './pages/Projects.jsx'
-import About from './pages/About.jsx'
-import Resume from './pages/Resume'
+
+const Home = lazy(() => import('./pages/Home.jsx'))
+const Projects = lazy(() => import('./pages/Projects.jsx'))
+const About = lazy(() => import('./pages/About.jsx'))
+const Resume = lazy(() => import('./pages/Resume'))
 
 const router = createHashRouter([
     {
         path: '/',
-        element: <Home />,
+        element: (
+            <Suspense fallback={<Loader />}>
+                <Home />
+            </Suspense>
+        ),
     },
     {
         path: '/about',
-        element: <About />,
+        element: (
+            <Suspense fallback={<Loader />}>
+                <About />
+            </Suspense>
+        ),
     },
     {
         path: '/projects',
-        element: <Projects />,
+        element: (
+            <Suspense fallback={<Loader />}>
+                <Projects />
+            </Suspense>
+        ),
     },
     {
         path: '/resume',
-        element: <Resume />,
+        element: (
+            <Suspense fallback={<Loader />}>
+                <Resume />
+            </Suspense>
+        ),
     },
 ])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <RouterProvider router={router} />
