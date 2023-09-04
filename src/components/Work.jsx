283d297a151636d0525c9839c90ffe47900ffac0
Faqu22 <facundoalvarez415@gmail.com>
2023-09-04 19:53:21 -0300

    Add Technologies and Status on Work component

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -5,16 +5,27 @@ import Button from './Button'
 
 function Work(props) {
     return (
-        <div className="project-card-view flex flex-col items-center">
+        <div className="project-card-view flex flex-col items-center relative">
             <img src={props.imgPath} alt="card-img" className="h-32 mt-6 " />
             <div className="flex-col items-center">
                 <p className="text-2xl mt-2">
                     {' '}
                     <strong>{props.title}</strong>
                 </p>
-                <p className="text-justify px-3">{props.description}</p>
-                <br />
-                <div className="flex justify-center space-x-6 ">
+                <p className="text-justify px-3 text-slate-200 h-60 overflow-hidden text-ellipsis">
+                    {props.description}
+                </p>
+                <h3 className="px-3 pt-3 text-white text-lg font-medium">Technologies:</h3>
+                <p className="px-3 text-slate-200">{props.technologies}</p>
+                <h3 className="pt-3 text-white text-lg font-medium">Status:</h3>
+                <span
+                    className={`text-justify pb-7 px-3 text-slate-200 text-base ${
+                        props.status === 1 ? 'text-green-500' : 'text-yellow-300'
+                    }`}
+                >
+                    {props.status === 0 ? 'In Process' : 'Finished'}
+                </span>
+                <div className="flex justify-center space-x-6 w-full bottom-2 absolute">
                     <Button
                         text="Github"
                         actionType="navigate"
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -9,16 +9,20 @@ function Projects() {
     const projectData = [
         {
             imgPath: Fixermate,
-            title: 'Fixermate',
+            title: 'FixerMate',
             description:
-                'FixerMate is an intuitive web application tailored for technicians and businesses in the mobile repair sector. Developed of Django-Ninja and the interactivity of ReactJS, it streamlines the management of device repairs, customer data, and even has a marketplace for spare parts. Real-time updates, efficient inventory management, and user-friendly interfaces make it an indispensable tool in the repair industry. In Process...',
+                'FixerMate is an intuitive web application tailored for technicians and businesses in the mobile repair sector. Developed of Django-Ninja and the interactivity of ReactJS, it streamlines the management of device repairs, customer data, and even has a marketplace for spare parts. Real-time updates, efficient inventory management, and user-friendly interfaces make it an indispensable tool in the repair industry.',
+            technologies: 'Python - Django Ninja - PostgreSQL - Docker',
+            status: 0,
             ghLink: 'https://github.com/TechWave-Organization/fixermate-backend',
         },
         {
             imgPath: Gieben,
             title: 'Gieben',
             description:
-                "Step into the world of digital sculpting with my unique landing page dedicated to plaster sculptures. Harnessing the power of Three.js, I've recreated 3D models that not only exhibit the beauty of the art form but also provide an interactive experience. Dive in and customize sections of the body, tweaking and tailoring as you see fit, offering a blend of traditional artistry and modern technology. In Process...",
+                "Step into the world of digital sculpting with my unique landing page dedicated to plaster sculptures. Harnessing the power of Three.js, I've recreated 3D models that not only exhibit the beauty of the art form but also provide an interactive experience. Dive in and customize sections of the body, tweaking and tailoring as you see fit, offering a blend of traditional artistry and modern technology.",
+            technologies: 'Vite - ReactJs - Javascript - ThreeJs - Tailwind CSS',
+            status: 0,
             ghLink: 'https://github.com/Faqu22/Gieben',
             demoLink: 'https://faqu22.github.io/Gieben/',
         },
@@ -27,6 +31,8 @@ function Projects() {
             title: 'Portfolio',
             description:
                 "Dive into my portfolio to witness a blend of technical expertise and creativity. From robust backend solutions to  frontend designs, explore projects that mirror my passion for coding. Whether you're seeking collaboration or just curious, you're bound to find something that resonates.",
+            technologies: 'Vite - ReactJs - Javascript - Tailwind CSS',
+            status: 1,
             ghLink: 'https://github.com/Faqu22/Faqu22-portfolio',
             demoLink: 'https://faqu22.github.io/Faqu22-portfolio/',
         },
@@ -45,11 +51,13 @@ function Projects() {
                 </div>
                 <div className="flex justify-center">
                     {projectData.map((project, index) => (
-                        <div className="project-card flex w-1/3 h-auto" key={index}>
+                        <div className="project-card flex w-1/3 " key={index}>
                             <Work
                                 imgPath={project.imgPath}
                                 title={project.title}
                                 description={project.description}
+                                technologies={project.technologies}
+                                status={project.status}
                                 ghLink={project.ghLink}
                                 demoLink={project.demoLink}
                             />
